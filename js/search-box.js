60d5377449f63b61362d4298ab936088b3090224
Daniyal2108 <dk2527689@gmail.com>
2021-12-23 23:42:05 +0500

    Update search-box.js

--- js/search-box.js
+++ js/search-box.js
@@ -1,5 +1,6 @@
 var x = document.getElementById("navbarContent");
 document.getElementById("search-box").onclick = function(){
+    var x = document.getElementById("navbarContent");
     x.style.display.none;
     var a = document.createElement("<input type='search' id='search-here'>");
     a.style["width"] = 100;
